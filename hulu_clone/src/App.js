26c516ed80f2e61d3ba5a14091c86972010edb7a
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-09-05 14:48:32 -0700

    completed header with styling

--- hulu_clone/src/App.js
+++ hulu_clone/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Nav from "./Nav";
+import Results from "./Results";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Nav />
+      <Results />
+    </div>
+  );
 }
 
 export default App;
